84b5e273f62454afb9de3a0810c524141a7f10cf
afthegreat <abelferede916@gmail.com>
2025-05-07 16:25:22 +0300

    all the contact routes are protected

--- routes/conatactRoutes.js
+++ routes/conatactRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();  
-const { getContact, addContact,getContactById ,updateContact, deleteContact } = require("../controllers/contactController");  
+const { getContact, addContact
+    ,getContactById ,
+    updateContact, 
+    deleteContact 
+      } = require("../controllers/contactController");  
+const validateToken = require("../middleware/validateTokenHandler");
 
+router.use(validateToken)
 router.route('/').get(getContact).post(addContact);
 router.route('/:id').get(getContactById).put(updateContact).delete(deleteContact);
 
